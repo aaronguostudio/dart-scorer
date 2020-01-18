e3c20d7aff0657f9d4423d48ae95237dcd52cf64
weiyan <im.weiyan@foxmail.com>
2020-01-18 13:57:21 -0700

    homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ScoresPage from './pages/ScoresPage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route path='/scores' component={ScoresPage} />
+          <Route component={HomePage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
