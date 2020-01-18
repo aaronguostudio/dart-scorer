7d40428d9f2058f3589f9994a889905ed6251327
weiyan <im.weiyan@foxmail.com>
2020-01-18 12:42:10 -0700

    wei

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Button, Icon } from 'antd';
+import * as style from './styles.module.css';
+
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+});
 
 export default () => {
-  return ()
+  return (
+    <div className ={style.root}>
+      <IconFont type="icon-tuichu" />
+      <Button className={style.btn} size="large">New Game</Button>      
+      <Button className={style.btn} size="large">Join a Game</Button>  
+    </div>
+  )
 }
