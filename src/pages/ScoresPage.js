a3639767716e46c49482ebbb1c177512d56abbea
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-18 13:55:27 -0700

    Added static ScoresPage

--- src/pages/ScoresPage.js
+++ src/pages/ScoresPage.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import Players from '../components/Players';
+import React, { useState } from 'react';
+import Player from '../components/Player';
 import ScoreTracker from '../components/ScoreTracker';
 
 export default () => {
+  const [myPlayer, setMyPlayer] = useState({
+    name: 'Me',
+    score: 0
+  });
+  const [opponentPlayer, setOpponentPlayer] = useState({
+    name: 'You',
+    score: 0
+  });
+
+  const addScoreHandler = scoreToAdd => {
+    setMyPlayer({ ...myPlayer, score: myPlayer.score + scoreToAdd });
+  };
+
   return (
     <div>
-      <Players myPlayer="Me" opponentPlayer="You" />
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <ScoreTracker label="Me" value={0} />
-        <ScoreTracker label="Opponent" value={0} isDisabled />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 24
+        }}
+      >
+        <Player player={myPlayer} />
+        <span style={{ fontSize: 24, margin: 16 }}>vs</span>
+        <Player player={opponentPlayer} />
+      </div>
+      <div style={{ padding: 16 }}>
+        <ScoreTracker onAddScore={addScoreHandler} />
       </div>
     </div>
   );
