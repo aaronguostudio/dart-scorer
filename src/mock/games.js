80c79bb117de9a2523bb8d4fd6fba11833d6653b
aaronguostudio <aaronguostudio@gmail.com>
2020-01-18 14:13:47 -0700

    add mock data

---
+++ src/mock/games.js
@@ -0,0 +1,14 @@
+export const games = [
+  {
+    host: 'AG-STD',
+    guest: 'BJ-WEG'
+  },
+  {
+    host: 'AG-STD',
+    guest: 'BJ-WEG'
+  },
+  {
+    host: 'AG-STD',
+    guest: 'BJ-WEG'
+  }
+]
---
+++ src/mock/results.js
@@ -0,0 +1,10 @@
+export const games = [
+  {
+    host: 'AG-STD',
+    guest: 'BJ-WEG',
+    hostWin: true,
+    hostScore: 10,
+    guestScore: 23,
+    recordedAt: new Date('2020-01-12 12:12:12')
+  }
+]
