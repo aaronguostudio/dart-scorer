cc8fc00b210b2d141c05e4c29a2e8225dd0c54f2
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-18 12:58:53 -0700

    Used ScoreTracker components in ScoresPage

--- src/pages/ScoresPage.js
+++ src/pages/ScoresPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Players from '../components/ScoreTracker';
+import Players from '../components/Players';
+import ScoreTracker from '../components/ScoreTracker';
 
 export default () => {
   return (
     <div>
       <Players myPlayer="Me" opponentPlayer="You" />
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <ScoreTracker label="Me" value={0} />
+        <ScoreTracker label="Opponent" value={0} isDisabled />
+      </div>
     </div>
   );
 };
